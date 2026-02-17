4ce603708953aeb441cb9a6793825801e089c501
amber <amberprincessrosana05@gmail.com>
2026-02-17 17:40:15 +0800

    Added basic structure for Login and Signup screens

--- react-native-layout/App.js
+++ react-native-layout/App.js
@@ -1,15 +1,9 @@
 import { StyleSheet, Text, View } from 'react-native';
-import Button from './src/components/Button';
-import InputContainer  from './src/components/InputContainer';
+import Login from './src/screens/Login'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <InputContainer iconName="mail-outline" iconSize={32} placeholder="Email" />
-      <InputContainer placeholder="No icon input field sample" />
-      <InputContainer iconName="lock-closed-outline" secureTextEntry iconSize={32} placeholder="Password" />
-      <Button buttonText="ANY TEXT HERE" />
-    </View>
+    <Login></Login>
   );
 }
 
